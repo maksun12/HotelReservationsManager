ddddd60ef33e31f50cc2db5d8532883ddb320b86
maksun12 <maksun12@abv.bg>
2020-02-16 11:28:29 +0200

    Create

---
+++ HotelReservationsManager/Data/HotelReservationsManagerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelReservationsManager.Data
+{
+    public class HotelReservationsManagerDbContext : DbContext
+    {
+        const string connectionString = "Server=PC_11_303-PC\\SQLEXPRESS; Database=HotelReservationsManagerDB; Trusted_Connection=True";
+
+   
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
